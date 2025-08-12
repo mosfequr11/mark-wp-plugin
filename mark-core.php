6f1953fe47ade0b1c1fbe1d3d8942d27a0c6b74e
Md Mosfequr <mosfequr11@users.noreply.github.com>
2025-08-12 21:52:47 +0600

    add: count the post content word

--- mark-core.php
+++ mark-core.php
@@ -9,12 +9,42 @@
  * Author URI: https://mosfequr.com/
  * Text Domain: elementor
  */
-function mark_activation_hook(){
 
+// Activation hook
+function mark_activation_hook()
+{
+	// Code to run on activation
+	error_log(message: 'Plugin activated!');
 }
-register_activation_hook( __FILE__, array("mark_activation_hook","") );
+register_activation_hook(__FILE__, 'mark_activation_hook');
 
-function mark_deactivation_hook(){
-    
+// Deactivation hook
+function mark_deactivation_hook()
+{
+	// Code to run on deactivation
+	error_log('Plugin deactivated!');
 }
-register_deactivation_hook( __FILE__, array("mark_deactivation_hook","")
+register_deactivation_hook(__FILE__, 'mark_deactivation_hook');
+
+
+
+// Load plugin textdomain
+function wordcount_load()
+{
+	load_plugin_textdomain('mark-core', false, dirname(plugin_basename(__FILE__)) . '/languages');
+}
+add_action('plugins_loaded', 'wordcount_load');
+
+// Show word count after post title
+function mark_show_word_count_after_title($title, $id = null)
+{
+	if (is_singular('post') && in_the_loop() && !is_admin()) {
+		$content = get_post_field('post_content', $id);
+		$word_count = str_word_count(wp_strip_all_tags($content));
+
+		// Append word count to the title
+		$title .= ' <span style="font-size:14px; color:#888;">(' . sprintf(__('%s words', 'mark-core'), $word_count) . ')</span>';
+	}
+	return $title;
+}
+add_filter('the_title', 'mark_show_word_count_after_title', 10, 2);
